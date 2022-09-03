a7c3b5f7373c9792bf04aa2a5a0894451f9bd470
marlleyck <marlleyck@hotmail.com>
2022-09-03 12:19:05 -0300

    fix: resolved bug header

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,8 +2,9 @@ export const Main = () => {
     return (
         <>
             <div className="bg-bg-main bg-no-repeat bg-center bg-cover w-screen h-[calc(100vh_-_4rem)] opacity-50 z-0 relative"></div>
-            <main className="font-['Montserrat'] w-full h-[calc(100vh_-_4rem)] flex items-center justify-center z-1 absolute inset-y-0">
-                <h1 className="text-white text-3xl uppercase font-bold z-2">Nós iremos fazer a melhor pizza da sua <span className="text-orange-300 z-2">vida!</span></h1>
+            <main className="font-['Montserrat'] w-full h-[calc(100vh_-_4rem)] flex items-center justify-center flex-col z-1 absolute bottom-0">
+                <h1 className="text-white text-3xl uppercase font-bold z-2 text-center">Nós iremos fazer a melhor <br /> pizza da sua <span className="text-orange-300 z-2">vida!</span></h1>
+                <p className="text-white mt-2.5 text-xl text-center">Escolha seu sabor preferido e nossos cozinheiros irão fazer uma pizza <br /> como você nunca viu.</p>
             </main>
         </>
     )
