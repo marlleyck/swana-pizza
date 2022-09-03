1490eedeb2b13aeb6cda117f48eda36e589b650a
marlleyck <marlleyck@hotmail.com>
2022-09-03 13:09:42 -0300

    style: finalized footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,12 @@
 import {FaInstagram} from 'react-icons/fa'
+import {FaFacebook} from 'react-icons/fa'
 
 export const Footer = () => {
     return (
-        <footer w-screen>
+        <footer className='w-full flex items-center justify-center p-4 gap-4'>
             <h1>fotter</h1>
-            <FaInstagram color='white' size={30}/>
+            <FaInstagram color='white' size={30} className='cursor-pointer'/>
+            <FaFacebook color='white' size={30}  className='cursor-pointer'/>
         </footer>
     )
 }
