c8790870957a0450e453923877cc6fd21f94f007
marlleyck <marlleyck@hotmail.com>
2022-09-03 12:10:28 -0300

    feat: initialized main

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,10 @@
+export const Main = () => {
+    return (
+        <>
+            <div className="bg-bg-main bg-no-repeat bg-center bg-cover w-screen h-[calc(100vh_-_4rem)] opacity-50 z-0 relative"></div>
+            <main className="font-['Montserrat'] w-full h-[calc(100vh_-_4rem)] flex items-center justify-center z-1 absolute inset-y-0">
+                <h1 className="text-white text-3xl uppercase font-bold z-2">Nós iremos fazer a melhor pizza da sua <span className="text-orange-300 z-2">vida!</span></h1>
+            </main>
+        </>
+    )
+}
