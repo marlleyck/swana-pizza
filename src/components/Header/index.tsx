0bdc4dfcc13306db699e7ff736b4e8aa570d7a20
marlleyck <marlleyck@hotmail.com>
2022-09-03 11:12:47 -0300

    style: update font size

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return (
-        <header className="w-screen h-16 flex items-center justify-center bg-preto font-['Montserrat']">
+        <header className="w-screen h-16 flex items-center justify-center bg-preto font-['Montserrat'] text-lg">
             <div className="w-4/5 flex items-center justify-between">
                 <h2 className="text-slate-300">imagem-logo</h2>
                 <ul className="flex items-center justify-center gap-2 text-slate-300">
