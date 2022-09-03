db3b04e4c81a8cedb272e98760c4fad52cbe12ed
marlleyck <marlleyck@hotmail.com>
2022-09-03 10:48:59 -0300

    chore: add color config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'preto': '#16161A',
+      }
+    },
   },
   plugins: [],
 }
