1e5e513644347273a8d455ab48622a6085d48947
marlleyck <marlleyck@hotmail.com>
2022-09-05 22:15:01 -0300

    style: header style changed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,35 @@
+import { Link } from "react-router-dom"
+
 export const Header = () => {
+    const url = String(window.location.href)
+
     return (
         <header className="w-full h-16 flex items-center justify-center bg-preto font-['Montserrat'] text-lg">
             <div className="w-4/5 flex items-center justify-between">
                 <h2 className="text-slate-300">imagem-logo</h2>
-                <ul className="flex items-center justify-center gap-2 text-slate-300">
-                    <li className="text-orange-300 cursor-pointer">Home</li>
-                    <li className="hover:text-orange-300 duration-200 cursor-pointer">Products</li>
-                    <li className="hover:text-orange-300 duration-200 cursor-pointer">Cart</li>
+                <ul className="flex items-center justify-center gap-5 text-slate-300">
+                    <Link to='/'>
+                        <li 
+                            className="text-orange-300 cursor-pointer">
+                            Home
+                        </li>
+                    </Link>
+
+                    <Link to='/options'>
+                        <li 
+                            className="hover:text-white duration-100 cursor-pointer"
+                            style={url.includes('/options') ? {fontWeight: 'bold', color: 'white'} : {}}>
+                            Pizzas
+                        </li>
+                    </Link>
+
+                    <Link to='/cart'>
+                        <li 
+                            className="hover:text-white duration-100 cursor-pointer"
+                            style={url.includes('/cart') ? {fontWeight: 'bold', color: 'white'} : {}}>
+                            Carrinho
+                        </li>
+                    </Link>
                 </ul>
             </div>
         </header>
