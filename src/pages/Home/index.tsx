d284fab240c1622d730c6bae96a45a9dffcbaeba
marlleyck <marlleyck@hotmail.com>
2022-09-05 22:15:48 -0300

    chore: add pages

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "../../components/Footer"
+import { Header } from "../../components/Header"
+import { Main } from "./Main"
+
+export const Home = () => {
+    return (
+        <>
+            <Header />
+            
+            <Main />
+
+            <Footer />
+        </>
+    )
+}
---
+++ src/pages/Options/index.tsx
@@ -0,0 +1,16 @@
+import { Footer } from "../../components/Footer"
+import { Header } from "../../components/Header"
+
+export const Options = () => {
+    return (
+        <>
+            <Header />
+
+            <main>
+                
+            </main>
+
+            <Footer />
+        </>
+    )
+}
