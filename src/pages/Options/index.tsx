65dd2ce74113c0819a8a07d6bf940442d7fee5a5
marlleyck <marlleyck@hotmail.com>
2022-09-08 11:53:34 -0300

    style: changed style options

--- src/pages/Options/index.tsx
+++ src/pages/Options/index.tsx
@@ -22,49 +22,49 @@ export const Options = () => {
                 <div className="w-full h-full grid grid-cols-4 justify-items-center content-center gap-10">
                     <div className="text-center text-white font-bold text-lg cursor-pointer font-['Montserrat']">
                         <img src={Pizza1} alt="pizza-1" 
-                            className="w-48 hover:scale-105 mb-4"/> 
+                            className="w-40 hover:scale-105 mb-4"/> 
                         <p>Portuguesa</p>
                         
                         <Button name='Adicionar' />
                     </div>
                     <div className="text-center text-white font-bold text-lg cursor-pointer font-['Montserrat']">
                         <img src={Pizza2} alt="pizza-2" 
-                            className="w-48 hover:scale-105 mb-4"/>
+                            className="w-40 hover:scale-105 mb-4"/>
                         <p>Gorgonzola</p>
                         
                         <Button name='Adicionar' />
                     </div>
                     <div className="text-center text-white font-bold text-lg cursor-pointer font-['Montserrat']">
                         <img src={Pizza3} alt="pizza-3" 
-                            className="w-48 hover:scale-105 mb-4" />
+                            className="w-40 hover:scale-105 mb-4" />
                         <p>Nordestina</p>
                         
                         <Button name='Adicionar' />
                     </div>
                     <div className="text-center text-white font-bold text-lg cursor-pointer font-['Montserrat']">
                         <img src={Pizza4} alt="pizza-4" 
-                            className="w-48 hover:scale-105 mb-4"/>
+                            className="w-40 hover:scale-105 mb-4"/>
                         <p>Quatro Queijos</p>
                         
                         <Button name='Adicionar' />
                     </div>
                     <div className="text-center text-white font-bold text-lg cursor-pointer font-['Montserrat']">
                         <img src={Pizza5} alt="pizza-5" 
-                        className="w-48 hover:scale-105 mb-4"/>
+                        className="w-40 hover:scale-105 mb-4"/>
                         <p>Calabresa</p>
                         
                         <Button name='Adicionar' />
                     </div>
                     <div className="text-center text-white font-bold text-lg cursor-pointer font-['Montserrat']">
                         <img src={Pizza6} alt="pizza-6" 
-                        className="w-48 hover:scale-105 mb-4"/>
+                        className="w-40 hover:scale-105 mb-4"/>
                         <p>Dois Queijos</p>
                         
                         <Button name='Adicionar' />
                     </div>
                     <div className="text-center text-white font-bold text-lg cursor-pointer font-['Montserrat']">
                         <img src={Pizza7} alt="pizza-7" 
-                        className="w-48 hover:scale-105 mb-4"/>
+                        className="w-40 hover:scale-105 mb-4"/>
                         <p>Chocolate</p>
                         
                         <Button name='Adicionar' />
