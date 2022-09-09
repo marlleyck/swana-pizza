c25fb383c6c711fc82e155de5f2505c4abc53dc1
marlleyck <marlleyck@hotmail.com>
2022-09-09 12:42:08 -0300

    chore: add types files

---
+++ src/types/PizzaJson.ts
@@ -0,0 +1,11 @@
+export type PizzaJson = {
+    data: 
+        [
+            {
+                "id": number;
+                "name": string;
+                "description": string;
+                "img": string;
+            },
+        ]
+}
